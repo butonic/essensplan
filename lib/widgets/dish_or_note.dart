6807b97118f91c5df7316bca00bd2ddbf75cc385
Jörn Friedrich Dreyer <jfd@butonic.de>
2022-04-07 22:49:58 +0200

    remove commented code
    Signed-off-by: Jörn Friedrich Dreyer <jfd@butonic.de>

--- lib/widgets/dish_or_note.dart
+++ lib/widgets/dish_or_note.dart
@@ -9,14 +9,10 @@ class DishOrNoteWidget extends StatefulWidget {
   /// Called when the user taps a dish.
   final DishTapCallback onTap;
 
-  /// Called when the user taps a dish.
-  //final Widget noteSuffix;
-
   DishOrNoteWidget({
     Key? key,
     this.dish,
     required this.onTap,
-    //required this.noteSuffix,
   }) : super(key: key);
 
   @override
@@ -50,43 +46,6 @@ class _DishOrNoteWidgetState extends State<DishOrNoteWidget> {
             }),
       ));
     } else if (widget.dish!.note != null) {
-      /*if (editing) {*/ // TODO austauschen funktioniert nicht sauber. denn es gibt kein on loose focus
-      // alternative: gleichen Dialog wie editDish nutzen um auch notizen zu bearbeiten?
-      // alternative: long press auf note> eingabefeld unten einblenden
-      //  - die liste bleibt sichtbar, TextField kommt sich nicht mit den drag gestures in die quere
-
-      /*
-      text = TextField(
-        maxLines: null,
-        textAlign: TextAlign.center,
-        autofocus: false,
-        controller: TextEditingController(text: widget.dish.note),
-        style: TextStyle(
-          color: Colors.black87,
-          fontSize: 14,
-        ),
-        decoration: InputDecoration(
-            suffix: widget.noteSuffix,
-            isDense: true,
-            contentPadding: const EdgeInsets.all(4.0),
-            border: InputBorder.none,
-            hintText: 'Neue Notiz'),
-        onChanged: (value) {
-          if (widget.dish.note != value) {
-            widget.dish.note = value;
-            widget.dish.save();
-          }
-        },
-        //onTap: () {
-        //  widget.onTap(context, widget.dish);
-        //},
-      );
-
-        */
-
-      /*
-      } else {
-        */
       text = Center(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 4.0),
@@ -102,13 +61,7 @@ class _DishOrNoteWidgetState extends State<DishOrNoteWidget> {
               widget.onTap(context, widget.dish!);
             }),
       ));
-      /*}*/
     }
     return text;
-    /*return SizedBox(
-      width: double.infinity,
-      child: text,
-    );
-    */
   }
 }
